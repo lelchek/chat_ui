0150762bd6c5d12d575bc028705f9be0843d0999
Olha_Diachek <olha.diachek@litslink.com>
2023-04-27 14:09:13 -0400

    Refactoring typing animate

--- src/components/СhatContainer/components/AnimatedText/AnimatedText.tsx
+++ src/components/СhatContainer/components/AnimatedText/AnimatedText.tsx
@@ -1,61 +1,29 @@
 import { useState, useEffect, useRef } from 'react';
 import cn from 'classnames';
 import { CSSTransition } from 'react-transition-group';
-import { LINE_HEIGHT } from 'constants/sizes';
+import { LINE_HEIGHT, BACKGROUND_ANIMATE_TIME } from 'constants/general';
 
 import { AnimatedTextProps } from './types';
 import styles from './AnimatedText.module.scss';
 
 const AnimatedText = ({ text }: AnimatedTextProps) => {
   const textRef = useRef<HTMLParagraphElement>(null);
-  const [startAnimate, setStartAnimate] = useState<boolean>(false);
+
   const [maskRows, setMaskRows] = useState<number>(1);
-
-  const [animatedSecondRow, setAnimatedSecondRow] = useState<boolean>(false);
-  const [animatedThirdRow, setAnimatedThirdRow] = useState<boolean>(false);
-
-  const [animatedRowNumber, setAnimatedRowNumber] = useState<number>(1);
+  const [animatedRowNumber, setAnimatedRowNumber] = useState<number>(0);
 
   useEffect(() => {
-    if (!startAnimate) {
-      return;
-    }
-
-    const timeout = setTimeout(() => {
-      setAnimatedSecondRow(true);
-    }, 3000);
-
-    return () => {
-      clearTimeout(timeout);
-    };
-  }, [startAnimate]);
-
-  useEffect(() => {
-    if (!animatedSecondRow) {
-      return;
-    }
-
-    const timeout = setTimeout(() => {
-      setAnimatedThirdRow(true);
-    }, 3000);
-
-    return () => {
-      clearTimeout(timeout);
-    };
-  }, [animatedSecondRow]);
-
-  useEffect(() => {
-    const startAnimateTimeout = setTimeout(() => {
-      setStartAnimate(true);
-    }, 1700);
-
     const maskRowsTimeout = setTimeout(() => {
       const scrollHeight = textRef.current?.scrollHeight;
 
       const rows = scrollHeight ? scrollHeight / LINE_HEIGHT : 1;
 
-      setMaskRows(rows > 3 ? 3 : rows);
-    }, 1600);
+      setMaskRows(Math.ceil(rows));
+    }, BACKGROUND_ANIMATE_TIME + 100);
+
+    const startAnimateTimeout = setTimeout(() => {
+      setAnimatedRowNumber(1);
+    }, BACKGROUND_ANIMATE_TIME + 200);
 
     return () => {
       clearTimeout(startAnimateTimeout);
@@ -66,7 +34,7 @@ const AnimatedText = ({ text }: AnimatedTextProps) => {
   return (
     <div className={styles.root}>
       <p
-        className={cn(styles.text, { [styles.visible]: startAnimate })}
+        className={cn(styles.text, { [styles.visible]: animatedRowNumber > 0 })}
         ref={textRef}
       >
         {text}
@@ -74,30 +42,27 @@ const AnimatedText = ({ text }: AnimatedTextProps) => {
         <span
           className={styles.staticMask}
           style={{
-            height: LINE_HEIGHT * (maskRows - animatedRowNumber + 1),
-            top: LINE_HEIGHT * (animatedRowNumber - 1),
+            height: LINE_HEIGHT * (maskRows - animatedRowNumber),
+            top: LINE_HEIGHT * animatedRowNumber,
           }}
         />
 
         <span className={styles.animatedMmask}>
           {Array.from({ length: maskRows }, (_, index) => {
+            const currentRowNumber = index + 1;
+
             return (
               <CSSTransition
                 key={index}
-                in={
-                  index === 0
-                    ? startAnimate
-                    : index === 1
-                    ? animatedSecondRow
-                    : animatedThirdRow
-                }
-                timeout={3000 * maskRows}
+                in={currentRowNumber === animatedRowNumber}
+                timeout={3000}
                 classNames={{
                   enter: styles.gradientMaskEnter,
                   enterActive: styles.gradientMaskEnterActive,
                 }}
-                unmountOnExit
-                onEnter={() => setAnimatedRowNumber(index + 2)}
+                onEntered={() => {
+                  setAnimatedRowNumber(currentRowNumber + 1);
+                }}
               >
                 <span className={styles.gradientMask} />
               </CSSTransition>
---
+++ src/constants/general.ts
@@ -0,0 +1,3 @@
+export const LINE_HEIGHT = 24;
+
+export const BACKGROUND_ANIMATE_TIME = 1500;
